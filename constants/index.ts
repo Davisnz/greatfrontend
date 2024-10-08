bd00aa495e75c7750fafc8a46cd9eef07c73453e
Davisnz <Dens104@gmail.com>
2024-10-08 15:00:28 -0600

    Updated the images

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,12 @@ export const testimonialCard = [
   {
     name: "Sarah Dole",
     twitter: "@sarahdole",
-    profilePicture: "../app/challenge/testimonial-card/images/sarah.jpg",
+    image: {
+      src: "/images/testimonialCard/profile-thumbnail.png",
+      width: 48,
+      height: 48,
+      alt: "Sarah Dole profile picture",
+    },
     testimonial:
       "I've been searching for high-quality abstract images for my design projects, and I'm thrilled ot have found this platform. The variety and depth of creativity are astounding!",
   },
