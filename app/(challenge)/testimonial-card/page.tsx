e739127cfa095012793024ae2fe269ca11f97d83
Davisnz <Dens104@gmail.com>
2024-10-08 14:17:34 -0600

    Changed the card to pull from constants.ts

--- app/(challenge)/testimonial-card/page.tsx
+++ app/(challenge)/testimonial-card/page.tsx
@@ -1,28 +1,33 @@
 import Image from "next/image";
+import { testimonialCard } from "@/constants";
 
 const TestimonialCard = () => {
   return (
     <div className="w-[340px] rounded-lg bg-white p-6 text-black shadow-md">
-      <div className="flex flex-row">
-        <Image
-          src="/images/testimonialCard/profile-thumbnail.png"
-          width={48}
-          height={48}
-          alt="Profile thumbnail"
-          className="mb-3 rounded-full"
-        />
-        <div className="flex flex-col pl-2 ">
-          <p className="text-lg font-semibold leading-7">Sarah Dole</p>
-          <p className="text-sm font-normal leading-5 text-[#525252] ">
-            @sarahdole
+      {testimonialCard.map((testimonial) => (
+        <div key={testimonial.name}>
+          <div className="mb-4 flex items-center">
+            <Image
+              src={testimonial.image.src}
+              width={testimonial.image.width}
+              height={testimonial.image.height}
+              alt={testimonial.image.alt}
+              className="mr-3 rounded-full"
+            />
+            <div>
+              <p className="text-lg font-semibold leading-7">
+                {testimonial.name}
+              </p>
+              <p className="text-sm font-normal leading-5 text-[#525252]">
+                {testimonial.twitter}
+              </p>
+            </div>
+          </div>
+          <p className="text-base font-normal leading-6 text-[#525252]">
+            {testimonial.testimonial}
           </p>
         </div>
-      </div>
-      <p className="p-1 text-base font-normal leading-6 text-[#525252]">
-        I&apos;ve been searching for high-quality abstract images for my design
-        projects, and I&apos;m thrilled to have found this platform. The variety
-        and depth of creativity are astounding!
-      </p>
+      ))}
     </div>
   );
 };
