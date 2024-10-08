31a2785048755745de3226b0bd4e19505f4e071f
Davisnz <Dens104@gmail.com>
2024-10-08 13:50:48 -0600

    Updated homepage

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@ export const testimonialCard = [
   {
     name: "Sarah Dole",
     twitter: "@sarahdole",
-    profilePicture: "../app/challenge/testimonial-card/images/sarah.jpg",
+    profilePicture: "/images/testimonialCard/profile-thumbnail.png",
     testimonial:
       "I've been searching for high-quality abstract images for my design projects, and I'm thrilled ot have found this platform. The variety and depth of creativity are astounding!",
   },
