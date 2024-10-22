b9196617f13f7d3e10cd7ed3ea8b23cbe3b3245f
Davis <dens104@gmail.com>
2024-10-21 21:05:32 -0600

    clean state

--- app/(challenge)/buttons/page.tsx
+++ app/(challenge)/buttons/page.tsx
@@ -1,25 +1,7 @@
 import React from "react";
-import {
-  ButtonPrimaryMd,
-  ButtonPrimaryLg,
-  ButtonPrimaryXl,
-  ButtonPrimary2xl,
-} from "../components/buttons";
 
-const buttonsPage = () => {
-  return (
-    <main className="flex h-screen items-center justify-center">
-      <div className="flex flex-row items-center justify-center gap-4">
-        <ButtonPrimaryMd>Button CTA</ButtonPrimaryMd>
-
-        <ButtonPrimaryLg icon="/icons/star.svg">Button CTA</ButtonPrimaryLg>
-
-        <ButtonPrimaryXl icon="/icons/star.svg">Button CTA</ButtonPrimaryXl>
-
-        <ButtonPrimary2xl>Button CTA</ButtonPrimary2xl>
-      </div>
-    </main>
-  );
+const page = () => {
+  return <div>page</div>;
 };
 
-export default buttonsPage;
+export default page;
